627debb316b0125ded297ebe8ff5ecaa77ad9300
Christian Rojas <christian.rojas@utec.edu.pe>
2025-01-11 16:47:34 -0500

    backend allow port 3000 for frontend

--- xoc/settings.py
+++ xoc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5%f8bo$%w*d5c-1lsk+hg_i#2#u2mml1=cc-guu9e90&k2y1k-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'roles',
     'companies',
     'users',
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'xoc.urls'
@@ -134,3 +136,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Tu frontend React
+    "https://tudominio.com", # Si despliegas más adelante
+]
